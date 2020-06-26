0ba5d96930ea26053310ed5a68f035bae92551d9
Chloe Steffens <caforonda103@yahoo.com>
2020-06-26 17:53:30 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,17 @@
 class Team
   attr_reader :name, :motto
   
+  @@all = []
+  
   def initialize(params)
-    
+    @name = params[:name]
+    @motto = params[:motto]
+    @@all << self
   end
   
+  def self.all
+    @@all
+  end
   
   
 end
